53cb172f67488ac764e8258f07ca553938a029e3
felipe mascena <felipe.mascena.dev@gmail.com>
2025-02-24 17:35:33 -0300

    iniciando projeto

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,9 @@
+import ImgPrincipal from '../../images/img-initial.png';
+
+export default function Login() {
+  return (
+    <section>
+      <img src={ImgPrincipal} alt="Imagem de um Navio" />
+    </section>
+  );
+}
