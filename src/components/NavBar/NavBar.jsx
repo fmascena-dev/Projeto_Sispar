4bb6b41403188e687d58a8e93bda46dd03f6a7aa
felipe mascena <felipe.mascena.dev@gmail.com>
2025-02-25 00:45:10 -0300

    feat: finalizando estruturação da NavBar e iniciando estilização

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,44 @@
 import { FaUser } from 'react-icons/fa6';
 import { GoHome } from 'react-icons/go';
-import { MdHistory, MdOutlineContentPasteSearch, MdOutlineMenuOpen } from 'react-icons/md';
+import {
+  MdHistory,
+  MdOutlineContentPasteSearch,
+  MdOutlineMenuOpen,
+} from 'react-icons/md';
 import { RxExit } from 'react-icons/rx';
 import { TbFileDollar } from 'react-icons/tb';
 
 export default function NavBar() {
   return (
-    <>
-      <h1>NavBar</h1>
-      <MdOutlineMenuOpen />
-      <RxExit />
-      <FaUser />
-      <MdHistory />
-      <MdOutlineContentPasteSearch />
-      <TbFileDollar />
-      <GoHome />
-    </>
+    <section className='navbar'>
+
+      <div className="navbar__menu">
+        <MdOutlineMenuOpen />
+      </div>
+
+      <div className='navbar__user'>
+        <FaUser />
+      </div>
+
+      <nav className='navbar__links'>
+        <li>
+          <GoHome />
+        </li>
+        <li>
+          <TbFileDollar />
+        </li>
+        <li>
+          <MdOutlineContentPasteSearch />
+        </li>
+        <li>
+          <MdHistory />
+        </li>
+      </nav>
+
+      <div className='navbar__logout'>
+        <RxExit />
+      </div>
+      
+    </section>
   );
 }
