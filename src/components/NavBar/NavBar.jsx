c50304e63bb10bb69143da092c865fe7f8564e63
felipe mascena <felipe.mascena.dev@gmail.com>
2025-02-25 20:03:05 -0300

    feat: finalizando estruturação e estilização do NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,37 +8,55 @@ import {
 import { RxExit } from 'react-icons/rx';
 import { TbFileDollar } from 'react-icons/tb';
 
+import './NavBarStyles.scss';
+
 export default function NavBar() {
   return (
-    <section className='navbar'>
-
+    <section className="navbar">
       <div className="navbar__menu">
         <MdOutlineMenuOpen />
       </div>
 
-      <div className='navbar__user'>
-        <FaUser />
+      <div className="navbar__user">
+        <div className="user">
+          <FaUser />
+          <h2>Felipe Mascena</h2>
+          <p>Front-End Developer</p>
+        </div>
+
+        <nav className="navbar__links">
+          <ul>
+            <li>
+              <GoHome />
+              <a href="">
+                <p>Início</p>
+              </a>
+            </li>
+            <li>
+              <TbFileDollar />
+              <a href="">
+                <p>Reembolsos</p>
+              </a>
+            </li>
+            <li>
+              <MdOutlineContentPasteSearch />
+              <a href="">
+                <p>Análises</p>
+              </a>
+            </li>
+            <li>
+              <MdHistory />
+              <a href="">
+                <p>Histórico</p>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
 
-      <nav className='navbar__links'>
-        <li>
-          <GoHome />
-        </li>
-        <li>
-          <TbFileDollar />
-        </li>
-        <li>
-          <MdOutlineContentPasteSearch />
-        </li>
-        <li>
-          <MdHistory />
-        </li>
-      </nav>
-
-      <div className='navbar__logout'>
+      <div className="navbar__logout">
         <RxExit />
       </div>
-      
     </section>
   );
 }
