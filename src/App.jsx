a354f149e1b6bd0422f5df2ab3c0e06868251e8f
felipe mascena <felipe.mascena.dev@gmail.com>
2025-02-26 21:19:17 -0300

    feat: finalizando a estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-// import Login from './components/Login/Login';
-import MenuReembolso from './components/MenuReembolso/MenuReembolso';
+import Login from './components/Login/Login';
+// import MenuReembolso from './components/MenuReembolso/MenuReembolso';
+// import NavBar from './components/NavBar/NavBar';
 
 export default function App() {
   return (
     <>
-      {/* <Login /> */}
-      <MenuReembolso />
+      <Login />
+      {/* <NavBar /> */}
+      {/* <MenuReembolso /> */}
     </>
   );
 }
--- src/components/MenuReembolso/MenuReembolso.jsx
+++ src/components/MenuReembolso/MenuReembolso.jsx
@@ -1,20 +1,24 @@
+import './MenuReembolsoStyle.scss';
 import { AiOutlineClockCircle } from 'react-icons/ai';
-import { FaHome } from 'react-icons/fa';
 import { FiCornerUpLeft } from 'react-icons/fi';
+import { GoHome } from 'react-icons/go';
 import { IoIosArrowForward, IoIosList, IoMdClose } from 'react-icons/io';
+import { IoCloudDoneOutline } from 'react-icons/io5';
 import { VscHistory } from 'react-icons/vsc';
 
 export default function MenuReembolso() {
   return (
     <section className="menu__reembolso">
       <div className="header__reembolso">
-        <FaHome />
-        <IoIosArrowForward />
+        <GoHome />
+        <span>
+          <IoIosArrowForward />
+        </span>
         <p>Reembolsos</p>
       </div>
 
-      <section className="title__reembolso">
-        <div>
+      <section className='main__reembolso'>
+        <div className="title__reembolso">
           <h1>Sistema de Reembolsos</h1>
           <p>
             Solicite novos pedidos de reembolso, visualize solicitações em
@@ -31,7 +35,7 @@ export default function MenuReembolso() {
             <IoIosList />
             <p>Visualizar análises</p>
           </a>
-          <a href="#">
+          <a href="#" className='history'>
             <VscHistory />
             <p>Histórico</p>
           </a>
@@ -41,26 +45,27 @@ export default function MenuReembolso() {
           <ul>
             <li>
               {' '}
-              <FiCornerUpLeft /> <span>182</span> Solicitados
+              <FiCornerUpLeft className='arrow' /> <span>182</span> Solicitados
             </li>
             <li>
               {' '}
-              <AiOutlineClockCircle /> <span>74</span> Em análise
+              <AiOutlineClockCircle className='time' /> <span>74</span> Em análise
             </li>
             <li>
               {' '}
-              <img src="/check.svg" alt="" />
+              <img src="/check.svg" alt="" className='check'/>
               <span>195</span> Aprovados
             </li>
             <li>
               {' '}
-              <IoMdClose />
+              <IoMdClose className='reject' />
               <span>41</span> Rejeitados
             </li>
           </ul>
         </div>
 
         <div className="footer__reembolso">
+          <IoCloudDoneOutline />
           <p>Sistema atualizado</p>
         </div>
       </section>
